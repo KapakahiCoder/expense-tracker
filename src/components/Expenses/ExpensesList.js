e97bca6173777ca4e182541134dedd56472ddcd9
Kapakahi <rubbishman@gmail.com>
2022-11-10 23:41:10 +0900

    add chart bar functionality

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,9 +2,7 @@ import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
-const ExpensesList = (props) => {
-  let expensesContent = <p>No expenses found.</p>;
-
+const ExpensesList = (props) => {  
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
